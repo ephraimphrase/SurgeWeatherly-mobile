36c49c99f6425392d558a442258a8476b5b6243c
ephraimphrase <ephraimphrase@gmail.com>
2023-01-22 11:06:17 +0100

    Initial Commit

--- app/src/main/java/com/surgetech/surgeweatherly/ui/HomeScreen.kt
+++ app/src/main/java/com/surgetech/surgeweatherly/ui/HomeScreen.kt
@@ -1,11 +1,9 @@
-package com.surgetech.surgeweatherly.ui.theme
+package com.surgetech.surgeweatherly.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -17,12 +15,13 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.surgetech.surgeweatherly.R
+import com.surgetech.surgeweatherly.ui.theme.IconBlue
+import com.surgetech.surgeweatherly.ui.theme.NavyBlue
 
 @Composable
 fun HomeScreen() {
